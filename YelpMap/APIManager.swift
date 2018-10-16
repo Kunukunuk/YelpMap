ddfbd9f0325cd6cff6a99215f1cec2edafa76b44
Kun Huang <kkun.huang@gmail.com>
2018-10-16 04:32:43 -0400

    assets and map view that shows restaurants

---
+++ YelpMap/APIManager.swift
@@ -0,0 +1,49 @@
+//
+//  APIManager.swift
+//  YelpMap
+//
+//  Created by Kun Huang on 10/15/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreLocation
+
+class APIManager {
+    static let apiKey = "izCFqEx0usiPwAiv_ymJ4Sl2Lr_mpnN6U_VeEkn1iUyEUWLM2Rd76A6NlswCI-HlYVWYT2WYRFtNnD04lgageyBKPJkqDDA75C8UsJYwc7oXWMGDFSCRU93zoTBaW3Yx"
+    static let baseURLString = "https://api.yelp.com/v3/businesses/search"
+    var session: URLSession
+    
+    init() {
+        session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+    }
+    
+    func getRestaurants(latitude: CLLocationDegrees, longitude: CLLocationDegrees, completion: @escaping ([Restaurant]?, Error?) -> Void) {
+        
+        //let lat: Decimal = Decimal(location.coordinate.latitude)
+        //let long: Decimal = Decimal(location.coordinate.longitude)
+        /*let parameter: [String: AnyObject] = [
+                                            "term": "Restaurant" as AnyObject,
+                                            "location": "\(lat), \(long)" as AnyObject
+                                              ]
+        let data = try! JSONSerialization.data(withJSONObject: parameter)*/
+        let parameter = "term=Restaurant&location=\(latitude),\(longitude)&limit=50"
+        let url = URL(string: APIManager.baseURLString + "?" + parameter)!
+        var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        request.setValue("Bearer \(APIManager.apiKey)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+    
+                let restaurants = dataDictionary["businesses"] as! [[String: Any]]
+                
+                completion(Restaurant.restaurant(restaurantsDict: restaurants), nil)
+            } else {
+                completion(nil, error)
+            }
+        }
+        task.resume()
+    }
+}
--- YelpMap/MapViewController.swift
+++ YelpMap/MapViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 import MapKit
 import CoreLocation
 
+class customAnnotation: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    
+    init(pinTitle: String, location: CLLocationCoordinate2D) {
+        self.title = pinTitle
+        self.coordinate = location
+    }
+}
+
 class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    let locationManager = CLLocationManager()
+    var locationManager: CLLocationManager!
+    var updateCenter = false
+    var restaurants: [Restaurant]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         mapView.delegate = self
+        
+        locationManager = CLLocationManager()
         locationManager.requestWhenInUseAuthorization()
+        
         
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
@@ -27,15 +42,114 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
             locationManager.startUpdatingLocation()
         }
         
+        self.mapView.showsUserLocation = true
+        
+    }
+    
+    func createAnnotationOnMap() {
+        if let restaurants = restaurants {
+            for restaurant in restaurants {
+                addAnnotationAtAddress(address: restaurant.address!, title: restaurant.name!)
+            }
+        }
     }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
-            let span = MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03)
+            let span = MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015)
             let region = MKCoordinateRegion(center: location.coordinate, span: span)
-            mapView.setRegion(region, animated: true)
+            mapView.setRegion(region, animated: false)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
         }
     }
 
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        print("Finsihed")
+        updateCenter = true
+        let location = locationManager.location?.coordinate
+        APIManager().getRestaurants(latitude: (location?.latitude)!, longitude: (location?.longitude)!) { (rest: [Restaurant]?, error: Error?) in
+            if let error = error {
+                print("error: \(error.localizedDescription)")
+            } else {
+                print("successful")
+                self.restaurants = rest
+                self.createAnnotationOnMap()
+            }
+        }
+    }
+    
+    func removeAnnotation() {
+        let annotationsToRemove = mapView.annotations
+        mapView.removeAnnotations( annotationsToRemove )
+    }
+    
+    func addAnnotationAtAddress(address: String, title: String) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            if let placemarks = placemarks {
+                if placemarks.count != 0 {
+                    let coordinate = placemarks.first!.location!
+                    let annotation = MKPointAnnotation()
+                    let custom = customAnnotation(pinTitle: title, location: coordinate.coordinate)
+                    annotation.coordinate = coordinate.coordinate
+                    annotation.title = title
+                    self.mapView.addAnnotation(custom)
+                }
+            }
+        }
+    }
+    
+    /*func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        <#code#>
+    }*/
+    
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        removeAnnotation()
+    }
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        if updateCenter {
+            print("ending here")
+            restaurants?.removeAll()
+            let location = self.mapView.centerCoordinate
+            APIManager().getRestaurants(latitude: location.latitude, longitude: location.longitude) { (rest: [Restaurant]?, error: Error?) in
+                if let error = error {
+                    print("error*** :\(error.localizedDescription)")
+                } else {
+                    print("successfully")
+                    self.restaurants = rest
+                    self.createAnnotationOnMap()
+                }
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "customannotation"
+        
+        guard !(annotation is MKUserLocation) else {
+            return nil
+        }
+        
+        // custom image annotation
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        if (annotationView == nil) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            annotationView?.rightCalloutAccessoryView = UIButton(type: .infoLight)
+        }
+        else {
+            annotationView!.annotation = annotation
+        }
+        
+        annotationView!.image = UIImage(named: "Food")
+        
+        return annotationView
+    }
+    
     /*
     // MARK: - Navigation
 
